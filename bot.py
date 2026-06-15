d0a754890de40d0a0065a19829f42f1fae6de8af
PolarisBBB <deniska1597@yandex.ru>
2026-06-15 10:59:17 +1000

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,17 @@
+import os
+import requests
+
+TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+data = {
+    "chat_id": CHAT_ID,
+    "text": "✅ Бот работает!"
+}
+
+r = requests.post(url, data=data)
+
+print(r.status_code)
+print(r.text)
